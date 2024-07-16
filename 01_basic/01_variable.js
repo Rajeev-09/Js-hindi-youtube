943201882a8bedba2a297baf5e3f7ebc8fea4c97
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-16 14:52:49 +0000

    overview of variable

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -13,5 +13,5 @@ accountCity = "Delhi"
 
 console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
 /*
-* Pefer not to use var because of issue in block scope and functional scope 
+ Pefer not to use var because of issue in block scope and functional scope 
  */
