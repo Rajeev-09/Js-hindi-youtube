4d9d3c652a275f2d210cf78515bc01bcb0b61c3d
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-16 15:29:59 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";// treat all Js Code as newer version
+
+// alert(3+3) //e are using nodejs, not in a browser 
+
+console.log(3
+    +3);// code readability that should be high
+console.log("Rajeev");
+
+// number=2^53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value hai, 
+// undefined => not assign
+// symbol => unqiue
+// object
+
+console.log(typeof Object);//function
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined
+
