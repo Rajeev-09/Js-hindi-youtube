4f0ead65fabb9bad178d4afcd66c56147b80c7fe
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-17 03:36:01 +0000

    data type Conversion

---
+++ 01_basic/03_conversionOperator.js
@@ -0,0 +1,29 @@
+let score = "33abc";
+ 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/*
+"33" => number;
+"33abc" => NaN
+true => 1 false => 0
+*/
+
+let isLoggedIn = 2;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false 
+// "Rajeev" => true
+
+let someNumber = 334
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
