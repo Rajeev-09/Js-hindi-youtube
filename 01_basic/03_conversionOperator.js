78dee536da4bdcf5b9f0a0d9aa7656fcf44e5d31
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-17 11:05:56 +0000

    Conversion

--- 01_basic/03_conversionOperator.js
+++ 01_basic/03_conversionOperator.js
@@ -18,7 +18,7 @@ true => 1 false => 0
 let isLoggedIn = 2;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false 
@@ -26,4 +26,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 334
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************************************************Operations******************************************************
+
+let value = 34;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Rajeev"
+let str3 =str1 + str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
