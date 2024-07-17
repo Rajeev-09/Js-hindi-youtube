ae73039db4fa583a847b17dfafa6d40f812fbb54
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-17 15:38:56 +0000

    Datatype Summary

---
+++ 01_basic/datatype_Summary.js
@@ -0,0 +1,43 @@
+// there are two type of datatype is that mean how can be access the data from memory management and put into the data
+
+// primitive Datatype esma swallow copy milti the data ke liye agar change bhi hota h toh voh copy ma hi toh h
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 9007199254740991n;
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Refrence(Non Primitive) esma original copy milti h 
+
+// Array, Objects, Functions, 
+
+const heros = ["Shaktiman","Naagraj","doga"]
+
+{
+    name:"Rajeev";
+    age:21;
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+}
+
+console.log(myFunction);
+console.log(typeof myFunction);
+// agar function ka typeof nklte hn toh voh object f(x) hota h object f(x)
+
+
+
+
+
+// js is a dynamic type language that mean it not introduce the type 
