bf45ba860b532e008a024bfc145847d56f2d4bc8
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-20 08:21:54 +0000

    Math and Num

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,36 @@
+const score = 100
+
+const balance = new Number(3930);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 138.92359
+// console.log(otherNumber.toPrecision(3));//139
+// console.log(otherNumber.toPrecision(4));//138.9
+
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+
+// ************************************  Maths    ****************************************
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(61.8));
+// console.log(Math.ceil(45.0));// it is used for the always show the toppest value 
+// console.log(Math.floor(29.5));// it is used for the always show the lowest value
+// console.log(Math.max(2,3,5,67));
+// console.log(Math.min(3,3,5,62,1,));
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
