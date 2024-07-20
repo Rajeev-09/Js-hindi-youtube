a3a605b0ba6ed745f4c06c4bbbb924fc050814c2
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-20 16:30:42 +0000

    Date in Javascript

---
+++ 01_basic/07_date.js
@@ -0,0 +1,33 @@
+// date
+
+let myDate = new Date()
+// console.log(myDate.toString());//Sat Jul 20 2024 14:38:49 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString());//Sat Jul 20 2024
+
+// console.log(myDate.toLocaleString());//7/20/2024, 2:39:30 PM
+
+// console.log(typeof myDate); 
+
+ let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getFullYear());
+
+// console.log(`${newDate.getDate()} and the Time is ${newDate.getTime()}`);
+
+const toLocaleDate = Date.toLocaleString('Default',{
+    weekDay : "long",
+    month : "short",
+    minute : "2-digit",
+    myDate : "date"
+})
+
+console.log(toLocaleDate);
