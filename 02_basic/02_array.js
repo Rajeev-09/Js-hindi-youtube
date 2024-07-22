99e3db345313bd2eb7a5f34354c141c5fc9d0215
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-22 17:35:55 +0000

    about the Object

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -10,20 +10,21 @@ const all_heros = marvel_Heros.concat(dc_Heros)    //it can be combine the 2 arr
 
 
 const all_new_heros = [...marvel_Heros,...dc_Heros]
-console.log(all_new_heros);
+// console.log(all_new_heros);
 
 const another_array = [1,2,3,4,[5,6,7],8,[9,10,11,]]
 const real_another_array = another_array.flat(Infinity) //it can be flat array
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
-console.log(Array.isArray("Rajeev"));
-console.log(Array.from("Rajeev"));
+// console.log(Array.isArray("Rajeev"));
+// console.log(Array.from("Rajeev"));
 
+// Array.from({name : "Rajeev"})//Interesting Topic
 // Interview  related topic such as they use of (from) in this topic it can show the output is empty beacuse 
 // they undefined what the change in object then they show empty 
 
-Array.from({name : "Rajeev"})//Interesting Topic
+
 
 
 
@@ -31,6 +32,6 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));// Returns a new array from a set of elements.
+// console.log(Array.of(score1,score2,score3));// Returns a new array from a set of elements.
 
 
---
+++ 02_basic/03_object.js
@@ -0,0 +1,54 @@
+// Objects can create as a 2 ways, 1:> Construtor     2:> Literal
+
+// these are own word Create an Object is called a singleton//yeh apni trah ka ek hi object h
+
+// they doesn't make a Single when they using a literal 
+// construtor make the Singleton
+
+// Object literal 
+
+
+const mySym = Symbol("Key1")
+
+const Jsuser = {
+    name : "Rajeev",
+    age : "18",
+    [mySym] : "mykey1",
+    location : "Srinagar",
+    email : "rajeevrawat210@gmail.com",
+    isLoggedIn : "false",
+    lastLoginday : ["Monday","Tuesday","Friday"]
+}
+
+
+
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);// behind the scene object like as email, name is using like as a string 
+// then they show error 
+// console.log(Jsuser[mySym]);
+
+// Interview  like questions 
+// Take a symbol then they added in the object key and print the value  
+// Ans : Using the []brace then they print like as a symbol
+
+Jsuser.age = "22"
+// Object.freeze(Jsuser)//Prevents the modification of existing property attributes and values, 
+// and prevents the addition of new properties.
+// console.log(Jsuser);
+
+Jsuser.greeting = function () {
+    console.log("Hello Js User");  
+}
+
+Jsuser.greetingtwo = function () {
+    console.log(`Hello Js User, ${this.name}`);  
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingtwo());
+
+
+
+
+
+
