0c573797b6957272ff73f012e5be3b6b9faa5a2f
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-27 15:31:45 +0000

    concept about the Scopes in Function

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -7,12 +7,51 @@ if (true) {
     let a = 10 
     const b = 20
     // var c = 30
-    console.log("Inner: ",a);
+    // console.log("Inner: ",a);
 }
 
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// function one() {
+    
+//     const userName = "Rajeev Rawat"
+//     function two() {
+//         const website  = "youtube"
+//         console.log(userName);
+//     }
+
+//     // console.log(website);
+//     two()
+// }
+
+// one()
+
+if (true) { 
+    const userName = "Rajeev"
+    if (true) {
+        const website = " Youtube"
+        // console.log(userName + website);
+    }    
+    // console.log(website);
+}
+// console.log(userName);
+
+
+// ++++++++++++++++++++++++++++++++++++  intersting  +++++++++++++++++++++++++++++++++++++++++
+// their are two technic for declared the function
+
+function addone(num) {
+    return num + 1;
+}
+addone(5)
+
+const addTwo = function(num){
+    return num +  2;
+}
+addTwo(8)
