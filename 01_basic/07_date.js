7cb1b60ffc2c3ae9c2e9c4b194c2ac2878e2df0d
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-07-29 06:45:28 +0000

    all about the arrow function

--- 01_basic/07_date.js
+++ 01_basic/07_date.js
@@ -8,9 +8,10 @@ let myDate = new Date()
 
 // console.log(typeof myDate); 
 
- let myCreatedDate = new Date(2023, 0, 23)
+//  let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate);
 
 let myTimeStamp = Date.now()
 // console.log(myTimeStamp);
@@ -30,4 +31,4 @@ const toLocaleDate = Date.toLocaleString('Default',{
     myDate : "date"
 })
 
-console.log(toLocaleDate);
+// console.log(toLocaleDate);
---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,56 @@
+const user = {
+    username: "Rajeev",
+    price: 999,
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to the website`);
+        // console.log(this);
+    }
+};
+// user.welcomeMessage();
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+// Interview Question : that broWser ke ander global object Window hota h
+
+
+
+// function chai(){                  //this f(X) is undefined becaz this keyWord used in only object
+//     const username =  "Rajeev"
+//     console.log(this.username);      
+// }
+// chai()
+
+const chai =  () => {
+    const username = "Rajeev"
+    console.log(this.username);
+}
+// chai()
+
+
+
+// Arrow function 
+// Emplicity Return that mean use of return keyWord that is emplicit return
+
+
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;
+// } 
+
+// Implicity Return given below that mean don't use of return keyWord that is not use in implicit
+
+// const addTwo = (num1,num2) => num1 + num2;
+// const addTwo = (num1,num2) => (num1 + num2); // agar hm sirf bracs ke use krta hn toh return use nhi kr skte thik h
+// baki cruly bracs ma toh use hta h return likna hi pd gya
+
+// agar hm implicity object create krn h toh tab hmko 
+const addTwo = (num1,num2) => ({username:"RAjeev"})
+
+console.log(addTwo(2,4));
+
+const myArr = [2,4,6,1,7]
+myArr.forEach(() => ({}));
+myArr.forEach(() => {})
