be2b14889a18a4e36b9149471fb65c77d4f7a47e
Rajeev-09 <rajeevrawat507@gmail.com>
2024-07-30 20:57:25 +0530

    all about the iife function

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,19 @@
+// (function chai() {
+//     console.log("DB Connected");    
+// })()
+
+// it mean (function call)(execution call)it mean first one is used to function call and second one is used to execution call
+
+// * Important :> Why it is used of iife :> that is becz global scope se problem hoti h kayi bar tab 
+// global declartion ke pollution ko htne ke liye use of iife 
+
+
+
+(function chai() {                  // that is a named iife 
+    console.log("DB Connected");    
+})();
+
+
+( (name) => {                            // that is simple iife
+    console.log(`DB connectedToo, ${name}`);
+} )('rajeev')
